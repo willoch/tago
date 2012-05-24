6f78bcb119078d252745e75c3bcd67661f3f9774
Thorbjorn Willoch <thwilloch@gmail.com>
2012-05-24 16:35:00 +0200

    more refactoring

--- src/tago.go
+++ src/tago.go
@@ -6,7 +6,7 @@
  Based on work done by:
  	Alex Combas 2010
 	 Initial release: January 03 2010
- Update Mai 22 2012
+ Updated May 22 2012
 */
 
 package main
@@ -20,73 +20,61 @@ import (
 	"go/parser"
 	"go/token"
 	"io"
+	"log"
 	"os"
 	"unicode/utf8"
 )
 
-// Get working directory and set it for savePath flag default
-
-func whereAmI() string {
-	dir, err := os.Getwd()
-	if err != nil {
-		fmt.Printf("Error getting working directory: %s\n", err.Error())
-		return ""
-	}
-	return dir + "/"
-}
-
-// Setup flag variables
 var (
-	saveDir = flag.String("d", whereAmI(),
-				"Change save directory: -d=/path/to/my/tags/")
-	tagsName           = flag.String("o",
-		"TAGS", "Change TAGS name: -o=MyTagsFile")
-	appendMode         = flag.Bool("a", false, "Append mode: -a")
-	fset               *token.FileSet
-	contentCurrentFile []string
+	fullTag        = flag.Bool("f", false, "make package.type.Ident tags for receivers")
+	tagsName       = flag.String("o", "TAGS", "Change TAGS name: -o=MyTagsFile")
+	appendMode     = flag.Bool("a", false, "Append mode: -a")
+	fset           *token.FileSet
+	contentCurrent []string
 )
 
-type Tea struct {
+type Buffer struct {
 	bytes.Buffer
 }
 
 func LineAndPos(lineno int) (line string, pos int) {
-	for i := 0; i < lineno-1; i++ {
-		pos += utf8.RuneCountInString(contentCurrentFile[i])
+	for i, v := range contentCurrent {
+		pos += utf8.RuneCountInString(v)
+		if i == lineno-1 {
+			return v[:len(v)-1], pos
+		}
 	}
-	x := contentCurrentFile[lineno-1]
-	line = x[:len(x)-1]
 	return
 }
 
-// Writes a TAGS line to a Tea buffer
-
-func (t *Tea) drink(leaf *ast.Ident, pkgname, rcvname string) {
+// Writes a TAGS line to a Buffer buffer
+func (t *Buffer) tagLine(leaf *ast.Ident, pkgname, rcvname string) {
 	P := fset.Position(leaf.NamePos)
 	n, l := leaf.String(), P.Line
 	s, o := LineAndPos(P.Line)
+	beforedot := pkgname
 	if rcvname != "" {
-		pkgname = rcvname
+		beforedot = rcvname
 	}
-	fmt.Fprintf(t, "%s%s.%s%d,%d\n", s, pkgname, n, l, o)
+	if rcvname != "" && *fullTag {
+		fmt.Fprintf(t, "%s\177%s.%s.%s\001%d,%d\n", s, pkgname, rcvname, n, l, o)
+	} else {
+		fmt.Fprintf(t, "%s\177%s.%s\001%d,%d\n", s, beforedot, n, l, o)
+	}
 }
 
-// TAGS file is either appended or created, not overwritten.
-func (t *Tea) savor() {
-	location := fmt.Sprintf("%s%s", *saveDir, *tagsName)
-	flag := os.O_WRONLY
+func getFile() *os.File {
+	flag := os.O_WRONLY | os.O_CREATE
 	if *appendMode {
 		flag |= os.O_APPEND
 	} else {
-		flag |= os.O_CREATE | os.O_TRUNC
+		flag |= os.O_TRUNC
 	}
-	file, err := os.OpenFile(location, flag, 0666)
-	if err != nil {
-		fmt.Println(`Error opening file "`, location, `": `, err.Error())
-		return
+	f, e := os.OpenFile(*tagsName, flag, 0666)
+	if e != nil {
+		log.Fatal(e)
 	}
-	file.Write(t.Bytes())
-	file.Close()
+	return f
 }
 
 func theReceiver(leaf *ast.FuncDecl) (ret string) {
@@ -106,85 +94,77 @@ func readCurrentFile(name string) (ret []string) {
 	ret = make([]string, 0, 2000)
 	file, err := os.Open(name)
 	if err != nil {
-		fmt.Printf("Error opening file in readCurrentFile(%s): %s\n",
-			name, err.Error())
-		return nil
+		log.Fatalf("Error opening file in readCurrentFile(%s): %s\n", name, err)
 	}
 	defer file.Close()
 	r := bufio.NewReader(file)
 	for {
-		n, e := r.ReadString('\n')
-		if e == nil {
+		switch n, e := r.ReadString('\n'); e {
+		case io.EOF:
+			return
+		case nil:
 			ret = append(ret, n)
-			if len(n) == 0 {
-				break
-			}
-		} else {
-			if e != io.EOF {
-				fmt.Printf("Error reading: %v\n", e)
-			}
-			break
+		default:
+			fmt.Println("Error reading ", file, " : ", e)
+			return
 		}
 	}
-	return
 }
-
 
 // Parses the source files given on the commandline,
 // returns a TAGS chunk for each file
-func brew() string {
-	teaPot := Tea{}
-	for i := 0; i < len(flag.Args()); i++ {
-		fset = token.NewFileSet()
-		teaCup := Tea{}
-		ptree, perr := parser.ParseFile(fset, flag.Arg(i), nil, 0)
-		if perr != nil {
-			fmt.Println("Error parsing file ",
-				flag.Arg(i), ": ", perr.Error())
-			continue
-		}
-		contentCurrentFile = readCurrentFile(flag.Arg(i))
-		pkgName := ptree.Name.Name
-		for _, l := range ptree.Decls {
-			switch leaf := l.(type) {
-			case *ast.FuncDecl:
-				teaCup.drink(leaf.Name, pkgName,
-					theReceiver(leaf))
-			case *ast.GenDecl:
-				for _, c := range leaf.Specs {
-					switch cell := c.(type) {
-					case *ast.TypeSpec:
-						teaCup.drink(cell.Name, pkgName, "")
-					case *ast.ValueSpec:
-						for _, atom := range cell.Names {
-							teaCup.drink(atom, pkgName, "")
-						}
-					}
-				}
+func iterateGenDecl(leaf *ast.GenDecl, pkgName string, fileBuffer *Buffer) {
+	for _, c := range leaf.Specs {
+		switch cell := c.(type) {
+		case *ast.TypeSpec:
+			fileBuffer.tagLine(cell.Name, pkgName, "")
+		case *ast.ValueSpec:
+			for _, atom := range cell.Names {
+				fileBuffer.tagLine(atom, pkgName, "")
 			}
 		}
-		totalBytes := teaCup.Len()
-		P := fset.Position(ptree.Pos())
-		fmt.Fprintf(&teaPot, "\f\n%s,%d\n%s", P.Filename, totalBytes, &teaCup)
 	}
-	return teaPot.String()
+}
+
+func iterateParsedFile(ptree *ast.File, fileBuffer *Buffer) {
+	pkgName := ptree.Name.Name
+	for _, l := range ptree.Decls {
+		switch leaf := l.(type) {
+		case *ast.FuncDecl:
+			fileBuffer.tagLine(leaf.Name, pkgName, theReceiver(leaf))
+		case *ast.GenDecl:
+			iterateGenDecl(leaf, pkgName, fileBuffer)
+		}
+	}
+}
+
+func parseFiles(outfile *os.File) {
+	for _, file := range flag.Args() {
+		fset = token.NewFileSet()
+		ptree, perr := parser.ParseFile(fset, file, nil, 0)
+		if perr != nil {
+			log.Println("Error parsing file ", file, ": ", perr)
+			continue
+		}
+		contentCurrent = readCurrentFile(file)
+
+		fileBuffer := Buffer{}
+		iterateParsedFile(ptree, &fileBuffer)
+		totalBytes := fileBuffer.Len()
+		fmt.Fprintf(outfile, "\f\n%s,%d\n%s", file, totalBytes, &fileBuffer)
+	}
+}
+func init() {
+	log.SetFlags(0)
+	flag.Parse()
+	if flag.NArg() == 0 {
+		log.Fatalf("Usage: %s [-f] [-a] [-h] [-o TagsFile] source.go ...\n",
+			os.Args[0])
+	}
 }
 
 func main() {
-	flag.Parse()
-	if flag.NArg() == 0 {
-		fmt.Printf(
-			"Usage: %s [-a] [-h] [-d directory] [-o TagsFile] source.go ...\n",
-			os.Args[0])
-		return
-	}
-	tea := Tea{}
-	fmt.Fprint(&tea, brew())
-
-	// if the string is empty there were parsing errors, abort
-	if tea.String() == "" {
-		fmt.Println("Parsing errors experienced, aborting...")
-	} else {
-		tea.savor()
-	}
+	f := getFile()
+	defer f.Close()
+	parseFiles(f)
 }
